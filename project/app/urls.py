d54d898984960b8b637bb486934f666cac87402e
YPrahasith <lohit2552@gmail.com>
2022-05-10 13:25:14 +0530

    Added Static Cards

--- project/app/urls.py
+++ project/app/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
 
 
 urlpatterns = [
