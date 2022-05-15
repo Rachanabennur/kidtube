6c470147d20438555882fa1c972f5322c66ee3ac
Rachana Bennur <rachanapbennur@gmail.com>
2022-05-15 16:36:46 +0530

    login and homepage basic page setup

--- project/pages/urls.py
+++ project/pages/urls.py
@@ -1,9 +1,20 @@
 # pages/urls.py
+from unicodedata import name
+# from django import views
+from pages import views
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, LoginPageView, command, upload
 
 
 urlpatterns = [
     path("", HomePageView, name="home"),
-    path("about/", AboutPageView, name="about")
+    path("about/", AboutPageView, name="about"),
+    path('command/<int:id>', command, name="delete"),
+    path('upload', upload, name="upload"),
+#     path('login/', LoginPageView.as_view(
+#     authentication_form=CustomLoginForm),
+#     name="login"
+#   )
+
+    path("", LoginPageView, name="login")
 ]
