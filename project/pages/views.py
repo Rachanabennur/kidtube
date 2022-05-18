4040cf0e5ebffcabfc5a4055f946ad30909270f5
YPrahasith <lohit2552@gmail.com>
2022-05-19 03:17:37 +0530

    Added DBMS to dynamically load cards

--- project/pages/views.py
+++ project/pages/views.py
@@ -1,6 +1,6 @@
 import re
 from django.shortcuts import redirect, render
-from .models import Videos, Feed
+from .models import Feed
 from .forms import UploadForm
 
 class Cards:
@@ -10,81 +10,10 @@ class Cards:
         self.image = image
         self.description = description
 
-cards = [
-     {
-        "id": 1,
-        "title": "Doctor Strange : Multiverse of Maddness",
-        "image": "https://i.auto-bild.de/mdb/extra_large/65/a1-e91.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Movies",
-    },
-    {
-        "id": 2,
-        "title": "KGF 2",
-        "image": "https://i.auto-bild.de/mdb/extra_large/65/a1-e91.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Movies",
-    },
-    {
-        "id": 3,
-        "title": "SpiderMan No Way Home",
-        "image": "https://i.auto-bild.de/mdb/extra_large/99/2ergrantourer-a02.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Movies",
-    },
-    {
-        "id": 4,
-        "title": "National News Today",
-        "image": "https://i.auto-bild.de/mdb/extra_large/99/2ergrantourer-a02.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"News",
-    },
-    {
-        "id": 5,
-        "title": "Software Project Management Tutorial",
-        "image": "https://i.auto-bild.de/mdb/extra_large/65/a1-e91.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Education",
-    },
-    {
-        "id": 6,
-        "title": "Software Engineering Tutorial",
-        "image": "https://i.auto-bild.de/mdb/extra_large/65/a1-e91.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Education",
-    },
-    {
-        "id": 7,
-        "title": "ICC Cricket WC ",
-        "image": "https://i.auto-bild.de/mdb/extra_large/65/a1-e91.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Sports",
-    },
-    {
-        "id": 8,
-        "title": "Thomas Cup Highlights",
-        "image": "https://i.auto-bild.de/mdb/extra_large/65/a1-e91.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Sports",
-    },
-    {
-        "id": 9,
-        "title": "Valorant Gaming Technique",
-        "image": "https://i.auto-bild.de/mdb/extra_large/65/a1-e91.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Gaming",
-    },
-    {
-        "id": 10,
-        "title": "Elephant farts on Human",
-        "image": "https://i.auto-bild.de/mdb/extra_large/65/a1-e91.png",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur expedita placeat tempora quam! Nisi, similique, corrupti magnam unde tempore, necessitatibus aliquid illo nesciunt consequatur odit maxime voluptates voluptas illum.",
-        "category":"Comedy",
-    },
 
-  ]
     # return render(request,'home.html', cardsInfo)
 def HomePageView(request):
+    cards = Feed.objects.all()
     return render(request,'home.html', context={"cards": cards})
 
 
@@ -92,6 +21,7 @@ def index(request):
     return render(request, 'index.html')
 
 def command(request, id):
+    cards = Feed.objects.all()
     for card in cards:
         if id == card["id"]:
             cards.remove(card)
@@ -100,21 +30,17 @@ def command(request, id):
 def upload(request):
     form = UploadForm()
     if request.method == "POST" :
-        form = UploadForm(request.POST, request.FILES)
-        title = form.data["title"]
-        print(title)
-        desc = form.data["description"]
-        print(desc)
-        long_desc = form.data["long_desc"]
-        print(long_desc)
-        img = form.data["img"]
-        print(img)
-        vid = form.data["vid"]
-        return redirect(HomePageView)
+        form = UploadForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            print("Saved")
+            return redirect(HomePageView)
+        
     return render(request,'upload.html', context={"form": UploadForm})
 
 def videoplay(request):
-  return render(request, 'videoplay.html', context={"cards":cards})  
+    cards = Feed.objects.all()
+    return render(request, 'videoplay.html', context={"cards":cards})  
 
 
 def AboutPageView(request):
